6179babdbe439843a70875401b9d879e5ce325fd
Kyle Oros <kyleblissoros@gmail.com>
2023-10-25 18:53:11 -0700

    Pushing routes written in tag-routes.js

--- fantastic-umbrella/Develop/routes/api/tag-routes.js
+++ fantastic-umbrella/Develop/routes/api/tag-routes.js
@@ -3,26 +3,85 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
+  try {
+    const tags = await Tag.findAll();
+    res.json(tags);
+  } catch (error) {
+    res.status(500).json({ error: 'Error in retrieving tags.' });
+  }
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const tagId = req.params.id;
+    const tag= await Tag.findByPk(tagId);
+
+    if (tag) {
+      res.json(tag);
+    } else {
+      res.status(404).json( {error: 'Tag not found. '});
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error in locating tag.' });
+  }
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const { name, description } = req.body;
+
+    const newTag = await Tag.create({
+      name,
+      description,
+    });
+    res.status(201).json(newTag);
+  } catch (error) {
+    res.status(500).json({ error: 'Error in creating a new tag.' });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const tagId = req.params.id;
+    const { name } = req.body;
+
+    const tag = await Tag.findByPk(tagId);
+
+    if (tag) {
+      tag.name = name;
+      await tag.save();
+
+      res.json(tag);
+    } else {
+      res.status(404).json({ error: 'Error in locating tag.' });
+    } 
+  } catch (error) {
+    res.status(500).json({ error: 'Error in updating tag.' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagId = req.params.id;
+    const tag= await Tag.findByPk(tagId);
+
+    if (tag) {
+      await tag.destroy();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ error: 'Error in locating tag.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error in deleting tag.' });
+  }
 });
 
 module.exports = router;
